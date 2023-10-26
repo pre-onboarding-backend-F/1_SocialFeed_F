ff0db269f175b454bff7a9259c730933aca1eddb
haeseung <showui96@gmail.com>
2023-10-26 19:43:13 +0900

    feat: 회원가입 api
    유저 create API 완성
    
    feattrue-\#8

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,37 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, HttpException, HttpStatus, Post } from '@nestjs/common';
 import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('user')
 export class UserController {
     constructor(private readonly userService: UserService) {}
+
+    @Post()
+    async createUser(@Body() createUserDto: CreateUserDto) {
+        try {
+            const user = await this.userService.createUser(createUserDto);
+            return {
+                success: true,
+                message: '회원 가입 성공',
+                result: {
+                    email: user.email,
+                    account: user.account
+                }
+            };
+        }
+        catch(e) {
+            throw new HttpException({
+                success: false,
+                message: e.message,
+                error: {
+                    status: e.status,
+                    name: e.name
+                }
+            },
+            HttpStatus.BAD_REQUEST
+            );
+        }
+    }
+
+
 }
