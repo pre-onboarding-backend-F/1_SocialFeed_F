38370a04e0dbe624817e25dd4c991d5796fc0032
Junsung Choi <choiexe1@gmail.com>
2023-10-26 17:51:27 +0900

    fix: app.module.ts
    App Module에 process가 import 된 부분을 삭제하였고
    
    코드 전체적으로 prettier format 적용 했습니다.

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,4 +1,4 @@
-import { Column, CreateDateColumn, DeleteDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
 import { Min } from 'class-validator';
 
 @Entity()
