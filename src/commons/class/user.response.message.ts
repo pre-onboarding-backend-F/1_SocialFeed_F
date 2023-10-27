7f936ebd38b8ca8e79c7a57c7491b087f9d99132
Junsung Choi <choiexe1@gmail.com>
2023-10-27 23:47:28 +0900

    feat: 로그인 API, 공통 응답 처리
    - JWT 모듈 추가
    - Passport 관련 패키지 추가
    - JWT 관련 패키지 추가
    - JWT 예외처리 필터 추가
    - AT(access token) 및 RT(refresh token) 전략 및 가드 추가
    - GetUser 및 ResponseMessage 데코레이터 추가
    - 공통 응답 처리를 위한 인터셉터 추가
    - 공통 응답 형식을 따르기 위한 user/approve api 수정
    - UserResponseMessage Class 추가
    - validationSchema.ts 에 JWT 관련 속성 추가
    
    여러 부분 수정을 많이해서 위 내용 외에 누락된 부분이 있을 수 있습니다.
    
    feat-#6

---
+++ src/commons/class/user.response.message.ts
@@ -0,0 +1,7 @@
+export class UserResponseMessage {
+    static SIGN_UP = '회원 가입 성공';
+    static APPROVE = '회원 가입이 승인 되었습니다.';
+    static SIGN_IN = '로그인 성공';
+    static LOG_OUT = '로그아웃 성공';
+    static REFRESH = '토큰 새로고침 성공';
+}
