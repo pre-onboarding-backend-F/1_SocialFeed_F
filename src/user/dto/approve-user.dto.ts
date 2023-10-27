70c2d48ee165ec0130210411dc38a914090cf4db
cabbage556 <liberty556@gmail.com>
2023-10-27 13:03:38 +0900

    feat: ApproveUserDto 추가
    - CreateUserDto를 OmitType으로 상속(email 필드 제외)
    - signupCode 속성 추가
    - signupCode 유효성 검사 추가
    
    feat-#4

---
+++ src/user/dto/approve-user.dto.ts
@@ -0,0 +1,11 @@
+import { OmitType } from '@nestjs/swagger';
+import { CreateUserDto } from './create-user.dto';
+import { IsNotEmpty, IsNumberString, MaxLength, MinLength } from 'class-validator';
+
+export class ApproveUserDto extends OmitType(CreateUserDto, ['email'] as const) {
+    @IsNumberString()
+    @MinLength(6)
+    @MaxLength(6)
+    @IsNotEmpty()
+    signupCode: number;
+}
