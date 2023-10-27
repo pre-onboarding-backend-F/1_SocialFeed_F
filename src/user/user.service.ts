9e51900aea18fc0896f7c41d226cf0e19325ec82
cabbage556 <liberty556@gmail.com>
2023-10-27 14:31:44 +0900

    feat: 회원가입승인 API 추가
    - POST /user/approve 회원가입승인 API 추가
    - BadRequestException 시 공통 응답 형식 리턴(실패)
      - ApproveUserDto 유효성 검사 실패
      - 존재하지 않는 계정일 경우
      - 이미 가입승인된 계정인 경우
      - 패스워드가 일치하지 않는 경우
      - 인증코드가 일치하지 않는 경우
    - 모든 검사 통과 후 isCertify 칼럼 true로 수정
    - 공통 응답 형식 리턴(성공)
    
    feat-#4

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -4,6 +4,7 @@ import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
 import * as bcrypt from 'bcryptjs';
+import { ApproveUserDto } from './dto/approve-user.dto';
 
 @Injectable()
 export class UserService {
@@ -37,5 +38,43 @@ export class UserService {
         });
 
         return await this.userRepository.save(newUser);
+    }
+
+    async approveUser(approveUserDto: ApproveUserDto) {
+        const { account, password, signupCode } = approveUserDto;
+
+        const user = await this.checkUser(account);
+        if (!user) {
+            throw new BadRequestException('존재하지 않는 계정입니다.');
+        }
+
+        if (user.isCertify) {
+            throw new BadRequestException('이미 가입승인된 계정입니다.');
+        }
+
+        const isPasswordMatched = await bcrypt.compare(password, user.password);
+        if (!isPasswordMatched) {
+            throw new BadRequestException('패스워드가 일치하지 않습니다.');
+        }
+
+        const isSignupCodeMatched = user.signUpCode === parseInt(signupCode);
+        if (!isSignupCodeMatched) {
+            throw new BadRequestException('인증코드가 일치하지 않습니다.');
+        }
+
+        await this.userRepository.update(
+            {
+                id: user.id,
+            },
+            {
+                isCertify: true,
+            },
+        );
+
+        return {
+            success: true,
+            mesage: '회원 가입 승인 되었습니다.',
+            result: null,
+        };
     }
 }
