597d5ea59b18448879e4ee4fe26c7d0f58f55f41
cabbage556 <liberty556@gmail.com>
2023-10-30 02:45:06 +0900

    feat: 데이터베이스 시드 코드 추가
    - users.factory.ts: 랜덤 유저 생성 팩토리
    - posts.factory.ts: 랜덤 게시물 생성 팩토리
    - main.seeder.ts: 팩토리를 사용해 데이터베이스 시드를 실행
      - 랜덤 유저 데이터 10개 생성
      - 랜덤 게시물 데이터 50개 생성
    
    feat-#28

---
+++ src/dataSource.ts
@@ -0,0 +1,30 @@
+import { ConfigService } from '@nestjs/config';
+import { config } from 'dotenv';
+import { DataSource, DataSourceOptions } from 'typeorm';
+import { SeederOptions } from 'typeorm-extension';
+import { BaseEntity } from './commons/base.entity';
+import { User } from './user/entities/user.entity';
+import { UsersFactory } from './database/factories/users.factory';
+import { PostsFactory } from './database/factories/posts.factory';
+import MainSeeder from './database/seeds/main.seeder';
+import { Post } from './post/entities/post.entity';
+
+config({
+    path: `.development.env`,
+});
+const configService = new ConfigService();
+
+const options: DataSourceOptions & SeederOptions = {
+    type: 'mysql',
+    host: 'localhost',
+    port: configService.getOrThrow<number>('MYSQL_PORT'),
+    username: configService.getOrThrow('MYSQL_ROOT_USER'),
+    password: configService.getOrThrow('MYSQL_ROOT_PASSWORD'),
+    database: configService.getOrThrow('MYSQL_DATABASE'),
+    entities: [BaseEntity, User, Post],
+    factories: [UsersFactory, PostsFactory],
+    seeds: [MainSeeder],
+};
+const dataSource = new DataSource(options);
+
+export default dataSource;
---
+++ src/database/factories/users.factory.ts
@@ -0,0 +1,15 @@
+import { User } from '../../user/entities/user.entity';
+import { setSeederFactory } from 'typeorm-extension';
+import * as bcrypt from 'bcryptjs';
+
+export const UsersFactory = setSeederFactory(User, async (faker) => {
+    const user = new User();
+    user.email = faker.internet.email();
+    user.account = faker.internet.userName();
+    user.password = await bcrypt.hash(faker.string.alphanumeric({ length: 10 }) + faker.string.symbol(2), 10);
+    user.signUpCode = faker.number.int({
+        min: 100000,
+        max: 999999,
+    });
+    return user;
+});
