0249a5de8b80e3748c72b2d284d288aff746b499
cabbage556 <liberty556@gmail.com>
2023-10-30 03:38:57 +0900

    fix: 랜덤 유저 팩토리 account 필드 수정
    - .과 _ 가 포함되지 않도록, 소문자가 되도록 수정하였음
    
    fix-#35

--- src/database/factories/users.factory.ts
+++ src/database/factories/users.factory.ts
@@ -5,7 +5,7 @@ import * as bcrypt from 'bcryptjs';
 export const UsersFactory = setSeederFactory(User, async (faker) => {
     const user = new User();
     user.email = faker.internet.email();
-    user.account = faker.internet.userName();
+    user.account = faker.internet.userName().replace(/._/g, '').toLowerCase();
     user.password = await bcrypt.hash(faker.string.alphanumeric({ length: 10 }) + faker.string.symbol(2), 10);
     user.signUpCode = faker.number.int({
         min: 100000,
