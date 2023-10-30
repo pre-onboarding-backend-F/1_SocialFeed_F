d869ae9bfe949a697ed7d74486b752b65eb0b290
cabbage556 <liberty556@gmail.com>
2023-10-30 22:20:42 +0900

    fix: 가입승인코드 유효성 검사 실패 메세지 수정
    - 가입승인코드에 대한 각각의 유효성 검사 실패 메세지를 수정하였음
    
    fix-#60

--- src/user/dto/approve-user.dto.ts
+++ src/user/dto/approve-user.dto.ts
@@ -3,13 +3,9 @@ import { CreateUserDto } from './create-user.dto';
 import { IsNotEmpty, IsNumberString, MaxLength, MinLength } from 'class-validator';
 
 export class ApproveUserDto extends OmitType(CreateUserDto, ['email'] as const) {
-	@IsNumberString()
-	@MinLength(6, {
-		message: '가입승인코드는 6자리입니다.',
-	})
-	@MaxLength(6, {
-		message: '가입승인코드는 6자리입니다.',
-	})
-	@IsNotEmpty()
+	@IsNumberString({}, { message: '가입승인코드는 정수형 문자열입니다.' })
+	@MinLength(6, { message: '가입승인코드는 6자리입니다.' })
+	@MaxLength(6, { message: '가입승인코드는 6자리입니다.' })
+	@IsNotEmpty({ message: '가입승인코드는 필수 입력 필드입니다.' })
 	signupCode: string;
 }
