e47849bc36cd17cde108918bbc39912e0a599a0c
cabbage556 <liberty556@gmail.com>
2023-10-30 14:05:07 +0900

    fix: DB 시드 코드
    - users.factory.ts
      - 이메일과 계정에 포함되던 ._- 특수문자 제외
    - posts.factory.ts
      - 랜덤 해시태그 데이터 생성 코드 제거
    - main.seeder.ts
      - 랜덤 게시물 데이터 생성 시 해시태그에 유저 계정을 첫번째 원소를 포함시킴
    
    fix-#40

--- src/database/factories/users.factory.ts
+++ src/database/factories/users.factory.ts
@@ -4,8 +4,8 @@ import * as bcrypt from 'bcryptjs';
 
 export const UsersFactory = setSeederFactory(User, async (faker) => {
 	const user = new User();
-	user.email = faker.internet.email();
-	user.account = faker.internet.userName().replace(/._/g, '').toLowerCase();
+	user.email = faker.internet.email().replace(/[._-]/g, '').toLowerCase();
+	user.account = faker.internet.userName().replace(/[._-]/g, '').toLowerCase();
 	user.password = await bcrypt.hash(faker.string.alphanumeric({ length: 10 }) + faker.string.symbol(2), 10);
 	user.signUpCode = faker.number.int({
 		min: 100000,
